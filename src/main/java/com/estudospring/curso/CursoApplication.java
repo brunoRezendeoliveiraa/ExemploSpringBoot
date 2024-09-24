862c2b9398eca6b7635d6c3b6961085431452824
Bruno R Oliveira <brunooliveiratecno@gmail.com>
2024-09-24 08:58:11 +0200

    Exemplo Spring Boot

---
+++ src/main/java/com/estudospring/curso/CursoApplication.java
@@ -0,0 +1,15 @@
+package com.estudospring.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+		
+		System.out.println("Olá mundo!");
+	}
+
+}
