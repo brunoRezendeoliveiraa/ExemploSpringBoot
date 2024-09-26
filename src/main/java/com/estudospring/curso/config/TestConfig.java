fc9a383e743deff41d7bf0063028d0ea540b7652
Bruno R Oliveira <brunooliveiratecno@gmail.com>
2024-09-26 16:56:50 +0200

    OrderStatus enum

--- src/main/java/com/estudospring/curso/config/TestConfig.java
+++ src/main/java/com/estudospring/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.estudospring.curso.entities.Order;
 import com.estudospring.curso.entities.User;
+import com.estudospring.curso.entities.enums.OrderStatus;
 import com.estudospring.curso.repositories.OrderRepository;
 import com.estudospring.curso.repositories.UserRepository;
 
@@ -32,9 +33,9 @@ public class TestConfig implements CommandLineRunner{
 		User u4 = new User(null, "Yasmim Oliveira", "yasmim@gmail.com", "977777777", "123456");
 		
 		//Formato da data ISO 8601
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITTING_PAYMENT, u3);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
---
+++ src/main/java/com/estudospring/curso/entities/enums/OrderStatus.java
@@ -0,0 +1,27 @@
+package com.estudospring.curso.entities.enums;
+
+public enum OrderStatus {
+
+	WAITTING_PAYMENT(1), PAID(2), SHIPPED(3), DELIVERED(4), CANCELED(5);
+
+	private int code;
+
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	// Static porque irá funcionar sem instanciar.
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+
+		throw new IllegalArgumentException("Invalid status code.");
+	}
+}
